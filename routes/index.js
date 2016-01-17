af558db665d2ca6b1f331d94cab1d588f6ef1729
zarmeen92 <zarmeennasim@yahoo.com>
2016-01-18 00:53:38 +0500

    Design front end task done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,52 @@
 var express = require('express');
 var router = express.Router();
-
+var needle = require('needle');
+var https = require('https');
+var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+ /* var headers = {
+     'kibo-app-id' : '5wdqvvi8jyvfhxrxmu73dxun9za8x5u6n59',
+     'kibo-app-secret': 'jcmhec567tllydwhhy2z692l79j8bkxmaa98do1bjer16cdu5h79xvx',
+     'kibo-client-id': 'cd89f71715f2014725163952'     
+  };
+  var options = {
+    headers: headers,
+    json:true
+ 
+  };
+ needle.get('https://api.kibosupport.com/users/allagents',options, function(error, response,body) {
+  if (!error && response.statusCode == 200)
+  {
+    var data  = JSON.parse(body);
+    console.log(data.agents);
+  }
+   else{
+      console.log('error');
+    }
+    });
+    
+   */
+
+   var options = {
+  url: 'https://api.kibosupport.com/users/allagents',
+  headers: {
+     'kibo-app-id' : '5wdqvvi8jyvfhxrxmu73dxun9za8x5u6n59',
+     'kibo-app-secret': 'jcmhec567tllydwhhy2z692l79j8bkxmaa98do1bjer16cdu5h79xvx',
+     'kibo-client-id': 'cd89f71715f2014725163952'     
+  }
+};
+ 
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    console.log(info.agents);
+    console.log(info);
+  }
+}
+ 
+request(options, callback);
+
   res.render('index', { title: 'Express' });
 });
 
