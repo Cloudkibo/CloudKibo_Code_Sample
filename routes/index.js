db2afcc50fb96f5123d2a987d07709f293bc83c6
zarmeen92 <zarmeennasim@yahoo.com>
2016-01-31 11:53:57 +0500

    testing and debugging

--- routes/index.js
+++ routes/index.js
@@ -37,7 +37,7 @@ router.get('/', function(req, res, next) {
         {
           data = null;
           console.log(error);
-          //res.render('groups',data);
+          res.render('could not fetch data');
         
         }
      }
